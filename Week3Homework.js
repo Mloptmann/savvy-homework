ffea5762caeba34fb1f41d2f90ab0c960876701e
Megan Loptmann <magloptmann@gmail.com>
2020-09-14 21:48:23 -0500

    h3

--- Week3Homework.js
+++ Week3Homework.js
@@ -1,7 +1,30 @@
-const pizzaToppings = ["Pepperoni", "Sausage", "Chicken", "Pineapple"];
+const pizzaToppings = [" Pepperoni", " Sausage", " Chicken", " Pineapple"];
 
-console.log(
-  "Welcome to the Pizza House we have",
-  pizzaToppings,
-  "for your topping options."
-);
+//Greet Customer
+function greetCustomer() {
+  return `Hello we offer${pizzaToppings.toString()} for your topping choices.`;
+}
+
+console.log(greetCustomer());
+
+//Get Pizza Order
+
+function getPizzaOrder(size, crustType, pizzaToppings) {
+  return `One ${size}, ${crustType} with${pizzaToppings} coming up!`;
+}
+
+console.log(getPizzaOrder("large", "Thin Crust", ...pizzaToppings));
+
+//Prepare Pizza
+function preparePizza(size, crustType, pizzaToppings) {
+  return `Cooking a ${size}, ${crustType} with${pizzaToppings}.`;
+}
+
+console.log(preparePizza("large", "Thin Crust", ...pizzaToppings));
+
+//Serve Pizza
+function servePizza(size, crustType, pizzaToppings) {
+  return `Here's your ${size}, ${crustType} with${pizzaToppings}. Enjoy!`;
+}
+
+console.log(servePizza("large", "Thin Crust", ...pizzaToppings));
